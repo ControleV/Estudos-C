f0f1caa8c6f156a807d94ecb2788f77c58834654
Victor <controleaoquadrado@gmail.com>
2025-08-14 14:04:19 -0300

    Variáveis e tipos de dados

---
+++ VariaveisETiposDeDados/script1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Os tipos precisam ser declarados junto com a variável.
+int main(){
+    int _idade = 25;         //Variáveis de número inteiro
+    int quantidade2 = 2;    
+    float altura = 1.75;       // Variáveis de ponto flutuante menores
+    double peso = 90.3;        // Variáveis de ponto flutuante maiores
+    char letra = 'A';         // Armazena caractéres
+    char nome[20] = "victor";      // Armazena strings via array de chars do tamanho 20
+
+    _idade = 23;                 // Definindo um novo valor
+}
