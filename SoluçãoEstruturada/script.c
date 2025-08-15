b40786416dc4c80f0cb09b800c479e9bc73f9bc5
Victor <controleaoquadrado@gmail.com>
2025-08-15 15:05:24 -0300

    Solução estruturada

---
+++ SoluçãoEstruturada/script.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void entradaDados() {
+    
+}
+
+float calcularMedia() {
+
+}
+
+void exibirMedia() {
+
+}
+
+int main() {
+    entradaDados();
+    float media = calcularMedia();
+    exibirMedia();
+    return 0;
+}
